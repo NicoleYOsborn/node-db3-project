84c7700b94c188d9379b0ee4f76d0fb2863f4fd5
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-07-28 18:02:32 -0700

    completed database methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,49 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id){
+    return db('schemes')
+    .where({id})
+    .first();
+}
+
+function findSteps(id) {
+    return db('steps as st')
+    .join('schemes as sc', 'st.scheme_id', "sc.id")
+    .select('st.id', "sc.scheme_name", "st.instructions")
+    .where({scheme_id: id});
+}
+
+function add(scheme) {
+    return db('schemes').insert(scheme)
+        .then(ids =>{
+            return findById(ids[0]);
+        });
+}
+
+function update(changes, id) {
+    return db("schemes")
+        .where({ id })
+        .update(changes)
+        .then(count =>{
+            return findById(id);
+        })
+}
+
+function remove(id) {
+    return db('schemes')
+    .where({ id })
+    .del();
+}
